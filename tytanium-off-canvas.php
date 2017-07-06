25c7842d465b618e330579354a3443e6b9b83471
Jonathan Trujillo <me@jontrujillo.com>
2017-07-05 17:18:50 -0700

    added main plugin file

---
+++ tytanium-off-canvas.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Plugin Name: Tytanium Off Canvas
+Plugin URI: https://github.com/TytaniumIdeas/tytanium-off-canvas
+Description: Plugin for creating an off-canvas navigation.
+Version: 1.0
+Author: Tytanium Ideas
+Author URI: https://www.tytaniumideas.com/
+License: MIT
+*/
+
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+  die;
+}
+
+
+class Tytnium_Off_Canvas{
+
+
+
+
+}
